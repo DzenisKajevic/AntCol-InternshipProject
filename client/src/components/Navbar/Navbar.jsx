37083b3fbeaaabca0be726a929e504d08aeafe0d
naimsecerovic <dev.nsecerovic@gmail.com>
2022-09-15 10:35:02 +0200

    added routes for links on the landing page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import './navbar.css';
-import '../../variables.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import "../../variables.css";
 
 const Navbar = () => {
-    return (
-        <header className='navbar-header'>
-            <nav className='navbar'>
-                <img className='music-app-logo' src="./assets/app-images/music-app-logo.png" alt="application logo" />
-                <button className='login-button shine'>Log in</button>
-                <button className='signup-button shine'>Sign up</button>
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header className="navbar-header">
+      <nav className="navbar">
+        <img
+          className="music-app-logo"
+          src="./assets/app-images/music-app-logo.png"
+          alt="application logo"
+        />
+        <Link to="/log-in" className="links">
+          <button className="login-button shine">Log in</button>
+        </Link>
+        <Link to="/registration" className="links">
+          <button className="signup-button shine">Sign up</button>
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
