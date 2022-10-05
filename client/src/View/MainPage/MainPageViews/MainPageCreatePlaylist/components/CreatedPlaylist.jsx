67b8b9dbf111f0f98d63e0494d26403b3303fc14
naimsecerovic <dev.nsecerovic@gmail.com>
2022-10-05 14:59:46 +0200

    icons

--- client/src/View/MainPage/MainPageViews/MainPageCreatePlaylist/components/CreatedPlaylist.jsx
+++ client/src/View/MainPage/MainPageViews/MainPageCreatePlaylist/components/CreatedPlaylist.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./createdPlaylist.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPen,
+  faTrash,
+  faLock,
+  faLockOpen,
+  faAnglesLeft,
+  faAnglesRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import * as mainAxios from "../../../mainAxios";
 import { setReloadPlaylists } from "../../../../../slices/audioVisualiser/songInfoSlice";
@@ -13,38 +20,47 @@ const CreatedPlaylist = ({ userImage, playlistName }) => {
 
   return (
     <div className="Some container idk">
-      {
-        playlists.map((playlist, index) => {
-          return (<div className="created-playlist-container" key={ playlist['_id'] }>
-            <img src={ userImage } alt="user image" className="user-image" />
-            <h1 className="playlist-name-title">{ playlist.playlistName }</h1>
-            <div className="edit-delete-playlist">
-              <FontAwesomeIcon icon={ faPen } className="edit-playlist" />
-              <FontAwesomeIcon icon={ faTrash } className="delete-playlist" onClick={
-                async () => {
-                  const result = await mainAxios.deletePlaylist(playlist['_id']);
+      {playlists.map((playlist, index) => {
+        return (
+          <div className="created-playlist-container" key={playlist["_id"]}>
+            {/* <img src={userImage} alt="user image" className="user-image" /> */}
+            <h1 className="playlist-name-title">{playlist.playlistName}</h1>
+            <FontAwesomeIcon icon={faAnglesRight} className="close-edit-menu" />
+            {/* <FontAwesomeIcon icon={faAnglesLeft} className="open-edit-menu" /> */}
+            <div className="edit-menu">
+              <FontAwesomeIcon icon={faLock} className="lock-playlist" />
+              <FontAwesomeIcon icon={faLockOpen} className="unlock-playlist" />
+              <FontAwesomeIcon icon={faPen} className="edit-playlist" />
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="delete-playlist"
+                onClick={async () => {
+                  const result = await mainAxios.deletePlaylist(
+                    playlist["_id"]
+                  );
                   if (result.data) {
                     const index = playlists.indexOf(playlist);
                     console.log(index);
                     dispatch(deletePlaylist(index));
                     dispatch(setReloadPlaylists(true));
                   }
-                }
-              } />
+                }}
+              />
             </div>
-          </div>);
-        })
-      }
-
-    </div >
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
-{/*       <img src={userImage} alt="user image" className="user-image" />
+{
+  /*       <img src={userImage} alt="user image" className="user-image" />
       <h1 className="playlist-name-title">{playlistName}</h1>
       <div className="edit-delete-playlist">
         <FontAwesomeIcon icon={faPen} className="edit-playlist" />
         <FontAwesomeIcon icon={faTrash} className="delete-playlist" />
-      </div> */}
+      </div> */
+}
 
 export default CreatedPlaylist;
